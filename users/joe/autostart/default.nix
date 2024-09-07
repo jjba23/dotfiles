d3dfa9042cb8858901ae5c4cededefb690e4d99a
Josep Bigorra <jjbigorra@gmail.com>
2024-09-07 11:31:56 +0200

    cc

--- users/joe/autostart/default.nix
+++ users/joe/autostart/default.nix
@@ -2,25 +2,24 @@
 let
   mkAutoStart = { binaryPath, exec, name, fullName ? name, description ? ""
     , inTerminal ? false, icon ? "org.gnome.Console", type ? "Application"
-    , categories ? [ ], version ? "1.0" }: {
-      home.file.".config/autostart/${name}.desktop".text = ''
-        [Desktop Entry]
-        Type=${type}
-        Version=${version}
-        Name=${fullName}
-        Comment=${description}
-        Path=${binaryPath}
-        Exec=${exec}
-        Icon=${icon}
-        Terminal=${inTerminal}
-        Categories=${lib.strings.intersperse ";" categories}
-      '';
-    };
-  myAutoStarts = [{
-    binaryPath = "/etc/profiles/per-user/joe/bin/systemctl";
-    exec = "systemctl --user start emacs";
+    , categories ? [ ], version ? "1.0" }: ''
+      [Desktop Entry]
+      Type=${type}
+      Version=${version}
+      Name=${fullName}
+      Comment=${description}
+      Path=${binaryPath}
+      Exec=${exec}
+      Icon=${icon}
+      Terminal=${inTerminal}
+      Categories=${lib.strings.intersperse ";" categories}
+    '';
+in {
+  home.file.".config/autostart/emacs-daemon.desktop".text = (mkAutoStart {
     name = "emacs-daemon";
     fullName = "Emacs Daemon";
-  }];
-  autoStarts = map mkAutoStart myAutoStarts;
-in autoStarts
+    binaryPath = "/etc/profiles/per-user/joe/bin/systemctl";
+    exec = "systemctl --user start emacs";
+  });
+
+}
