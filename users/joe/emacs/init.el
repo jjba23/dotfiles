352f7776f78f9ec518c5da9a3b26a479bf7f0119
Josep Bigorra <jjbigorra@gmail.com>
2024-09-21 23:38:05 +0200

    bkr

--- users/joe/emacs/init.el
+++ users/joe/emacs/init.el
@@ -45,26 +45,51 @@
 ;; Declare Joe's customizations
 
 (defcustom joe-font-mono "Iosevka Comfy Wide"
-  ""
+  "My personal choice for monospaced font family."
   :type 'string)
 
+(defun joe-set-base-faces ()
+  "Adjust the base Emacs faces to my preferences
+according to size, color and font family"
+  (set-face-attribute 'default nil
+    				  :height (round (tkngt 114))
+    				  :font joe-font-mono
+    				  )
+  (set-face-attribute 'mode-line nil
+    				  :height (tkngt 0.7)
+    				  :font joe-font-mono
+    				  )
+  (set-face-attribute 'mode-line-active nil
+    				  :height (tkngt 0.7)
+    				  :font joe-font-mono
+    				  )
+  (set-face-attribute 'mode-line-inactive nil
+    				  :height (tkngt 0.7)
+    				  :font joe-font-mono
+    				  )
+  )
+
 ;; Declare Joe's packages
-
-
 
 (use-package git-riddance
   :ensure (:host github :repo "jjba23/git-riddance.el" :branch "trunk"))
 
 (use-package tekengrootte
   :ensure (:host github :repo "jjba23/tekengrootte.el" :branch "trunk")
+  :bind (
+		 ("C-c f c" . tekengrootte-set-scale-colossal)
+		 ("C-c f j" . tekengrootte-set-scale-jumbo)
+		 ("C-c f x" . tekengrootte-set-scale-larger)
+		 ("C-c f l" . tekengrootte-set-scale-large)
+		 ("C-c f r" . tekengrootte-set-scale-regular)
+		 ("C-c f s" . tekengrootte-set-scale-small)
+		 ("C-c f t" . tekengrootte-set-scale-tiny)
+		 )
+  :hook (
+		 (tekengrootte-set-scale-hook . joe-set-base-faces)
+		 )
   :config
-  (defun gezichten-apply ()
-    ;; (set-face-attribute 'default
-    ;; 			:height (round (tkngt 114))
-    ;; 			:font joe-font-mono
-    ;; 			)
-    )
-  (gezichten-apply)
+  (joe-set-base-faces)
   )  
 
 (use-package ef-themes
@@ -75,9 +100,9 @@
   :ensure t
   :demand t
   :init
-  (setq auto-dark-polling-interval-seconds 4)
-  (setq auto-dark-allow-osascript nil)
-  (setq auto-dark-allow-powershell nil)
+  (setq auto-dark-polling-interval-seconds 4
+		auto-dark-allow-osascript nil
+		auto-dark-allow-powershell nil)
   :config
   (add-hook 'auto-dark-dark-mode-hook
             (lambda ()
@@ -85,7 +110,7 @@
               ))
   (add-hook 'auto-dark-light-mode-hook
             (lambda ()
-	      (load-theme 'ef-day t)
+			  (load-theme 'ef-day t)
               ))
   (auto-dark-mode t)
   )
@@ -95,7 +120,7 @@
   :demand t
   :init
   (setq vertico-cycle t
-	vertico-resize t)
+		vertico-resize t)
   :config
   (vertico-mode)
   )
@@ -116,15 +141,29 @@
   :ensure t
   :demand t)
 
+(use-package spacious-padding
+  :ensure t
+  :demand t
+  :init
+  (setq spacious-padding-widths
+		'( :internal-border-width 18
+           :header-line-width 2
+           :mode-line-width 2
+           :tab-width 4
+           :right-divider-width 2
+           :scroll-bar-width 8
+           :left-fringe-width 16
+           :right-fringe-width 16))
+
+
+  (setq spacious-padding-subtle-mode-line nil)
+  )
+
 (use-package which-key
   :ensure t
   :demand t)
 
 (use-package rainbow-mode
-  :ensure t
-  :demand t)
-
-(use-package org-tempo
   :ensure t
   :demand t)
 
@@ -163,9 +202,13 @@
         eshell-prompt-function 'epe-theme-pipeline)
   )
 
-(use-package f :ensure t :demand t)
+(use-package f
+  :ensure t
+  :demand t)
 
-(use-package speed-type :ensure t :demand t)
+(use-package speed-type
+  :ensure t
+  :demand t)
 
 (use-package dape
   :ensure t
@@ -182,10 +225,10 @@
   :ensure t
   :demand t
   :bind ( :map dired-mode-map (
-			       ("<TAB>" . dired-subtree-toggle)
-			       ("C-<tab>" . dired-subtree-toggle)
-			       ("C-<TAB>" . dired-subtree-toggle)
-			       ) ) 
+							   ("<TAB>" . dired-subtree-toggle)
+							   ("C-<tab>" . dired-subtree-toggle)
+							   ("C-<TAB>" . dired-subtree-toggle)
+							   ) ) 
   )
 
 (use-package dired-open-with :ensure t :demand t)
@@ -200,6 +243,55 @@
 
 (use-package emacs
   :ensure nil
+  :bind (("C-x C-b" . ibuffer)
+		 ("C-c a h" . highlight-compare-buffers)
+		 )
+  :hook (
+		 (text-mode . visual-line-mode)
+		 )
   :config
-  (setq ring-bell-function #'ignore)
+  (setq-default user-personal-name "Joe"
+				user-personal-full-name "Josep Jesus Bigorra Algaba"
+				user-personal-email "jjbigorra@gmail.com"
+				user-personal-initials "JJBA")
+
+  (setq org-todo-keywords
+		'((sequence "TODO" "WIP" "REVIEWING" "|" "DONE")))
+
+  (setq-default line-spacing 2
+				pgtk-wait-for-event-timeout 0
+				electric-indent-inhibit t)
+  
+  (setq treesit-font-lock-level 4
+		ring-bell-function #'ignore
+		frame-resize-pixelwise t
+		inhibit-startup-message t
+		backward-delete-char-untabify-method 'hungry
+		completion-cycle-threshold 3
+		tab-always-indent 'complete
+		text-mode-ispell-word-completion nil
+		read-extended-command-predicate #'command-completion-default-include-p
+		vc-follow-symlinks t
+		delete-by-moving-to-trash t
+		tab-width 4)
+  
+  (savehist-mode 1)
+  (tool-bar-mode -1)
+  (scroll-bar-mode -1)
+  (menu-bar-mode -1)
+  (delete-selection-mode +1)
+  
+  (set-frame-parameter nil 'alpha-background 90)
+  (add-to-list 'default-frame-alist '(alpha-background . 90))
+  
+  (when (fboundp 'windmove-default-keybindings) (windmove-default-keybindings))
+
+  (defalias 'yes-or-no-p 'y-or-n-p)
+  
+  (setq dired-listing-switches "-lAh --group-directories-first"
+		dired-kill-when-opening-new-dired-buffer t)
+
+  (add-hook 'dired-mode-hook (lambda () (dired-hide-details-mode 1)))
+
   )
+
