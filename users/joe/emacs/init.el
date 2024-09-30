2b7f22ac3cff3b4c5f641a28d4305dabd9cafe22
Josep Bigorra <jjbigorra@gmail.com>
2024-09-30 13:43:50 +0200

    mermaid

--- users/joe/emacs/init.el
+++ users/joe/emacs/init.el
@@ -374,6 +374,26 @@ According to size, color and font family"
 
 (use-package ob-mermaid :ensure t)
 
+(use-package mermaid-mode
+  :ensure t
+  :config
+  (setq mermaid-mode-map
+        (let ((map mermaid-mode-map))
+          (define-key map (kbd "C-c C-c") nil)
+          (define-key map (kbd "C-c C-f") nil)
+          (define-key map (kbd "C-c C-b") nil)
+          (define-key map (kbd "C-c C-r") nil)
+          (define-key map (kbd "C-c C-o") nil)
+          (define-key map (kbd "C-c C-d") nil)
+          (define-key map (kbd "C-c C-d c") 'mermaid-compile)
+          (define-key map (kbd "C-c C-d c") 'mermaid-compile)
+          (define-key map (kbd "C-c C-d f") 'mermaid-compile-file)
+          (define-key map (kbd "C-c C-d b") 'mermaid-compile-buffer)
+          (define-key map (kbd "C-c C-d r") 'mermaid-compile-region)
+          (define-key map (kbd "C-c C-d o") 'mermaid-open-browser)
+          (define-key map (kbd "C-c C-d d") 'mermaid-open-doc)
+          map)))
+
 (use-package page-break-lines :ensure t)
 
 (use-package orderless
